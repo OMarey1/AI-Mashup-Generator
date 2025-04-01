a43e2f35fcca20458282bd211061d2f5b35067cf
Omar Marey <45742304+OMarey1@users.noreply.github.com>
2025-04-01 12:18:32 +0200

    Update stem_separator.py

--- utils/stem_separator.py
+++ utils/stem_separator.py
@@ -3,7 +3,7 @@ from audio_separator.separator import Separator
 class StemProcessor:
     def __init__(self):
         self.separator = Separator()
-        self.separator.load_model(model_filename='UVR_MDXNET_KARA_2.onnx)
+        self.separator.load_model(model_filename='UVR_MDXNET_KARA_2.onnx')
     
     def process_track(self, file_path):
         return self.separator.separate(file_path)
